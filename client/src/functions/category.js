adf9b39846bdda512fc1f43c246e8a461e6d9232
bharath2612 <bharathchippa49@gmail.com>
2021-03-10 15:20:27 +0530

    55. Category CRUD requests

---
+++ client/src/functions/category.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+//list
+export const getCategories = async () =>
+  await axios.get(`${process.env.REACT_APP_API}/categories`);
+
+  //read
+export const getCategory = async (slug) =>
+  await axios.get(`${process.env.REACT_APP_API}/category/${slug}`);
+
+  //delete
+export const removeCategory = async (slug, authtoken) =>
+  await axios.delete(`${process.env.REACT_APP_API}/category/${slug}`, {
+    headers: {
+      authtoken,
+    },
+  });
+
+  //update
+export const updateCategory = async (slug, category, authtoken) =>
+  await axios.put(`${process.env.REACT_APP_API}/category/${slug}`, {
+    headers: {
+      authtoken,
+    },
+  });
+
+//create
+export const createCategory = async (category, authtoken) =>
+  await axios.post(`${process.env.REACT_APP_API}/category/${slug}`, category, {
+    headers: {
+      authtoken,
+    },
+  });
