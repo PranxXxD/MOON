c02b64c576f37021a9d20ca72db75ea77a7c77d3
bharath2612 <bharathchippa49@gmail.com>
2021-03-11 12:54:49 +0530

    59. Category Render and Delete

--- client/src/pages/admin/category/CategoryCreate.js
+++ client/src/pages/admin/category/CategoryCreate.js
@@ -7,13 +7,15 @@ import {
   getCategories,
   removeCategory,
 } from "../../../functions/category";
+import { Link } from "react-router-dom";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 
 const CategoryCreate = () => {
   const { user } = useSelector((state) => ({ ...state }));
 
   const [name, setName] = useState("");
   const [loading, setLoading] = useState(false);
-  const [categories, setCategories] = useState("");
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     loadCategories();
@@ -24,18 +26,41 @@ const CategoryCreate = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    // console.log(name);
     setLoading(true);
     createCategory({ name }, user.token)
       .then((res) => {
+        // console.log(res)
         setLoading(false);
         setName("");
         toast.success(`"${res.data.name}" is created`);
+        loadCategories();
       })
       .catch((err) => {
         console.log(err);
         setLoading(false);
-        if (err.statusCode === 400) toast.error(err.response.data);
+        if (err.response.status === 400) toast.error(err.response.data);
       });
+  };
+
+  const handleRemove = async (slug) => {
+    // let answer = window.confirm("Delete?");
+    // console.log(answer, slug);
+    if (window.confirm("Delete?")) {
+      setLoading(true);
+      removeCategory(slug, user.token)
+        .then((res) => {
+          setLoading(false);
+          toast.error(`${res.data.name} deleted`);
+          loadCategories();
+        })
+        .catch((err) => {
+          if (err.response.status === 400) {
+            setLoading(false);
+            toast.error(err.response.data);
+          }
+        });
+    }
   };
 
   const categoryForm = () => (
@@ -51,10 +76,11 @@ const CategoryCreate = () => {
           required
         />
         <br />
-        <button className="btn btn-outline-primary">SAVE</button>
+        <button className="btn btn-outline-primary">Save</button>
       </div>
     </form>
   );
+
   return (
     <div className="container-fluid">
       <div className="row">
@@ -63,16 +89,31 @@ const CategoryCreate = () => {
         </div>
         <div className="col">
           {loading ? (
-           <h4 className="text-danger">Loading...</h4>
+            <h4 className="text-danger">Loading..</h4>
           ) : (
-            <h4>Create Category</h4>
+            <h4>Create category</h4>
           )}
- 
           {categoryForm()}
-          {/* {JSON.stringify(categories)} */}
-          </div>
+          <hr />
+          {categories.map((c) => (
+            <div className="alert alert-secondary" key={c._id}>
+              {c.name}
+              <span
+                onClick={() => handleRemove(c.slug)}
+                className="btn btn-sm float-right"
+              >
+                <DeleteOutlined className="text-danger" />
+              </span>
+              <Link to={`/admin/category/${c.slug}`}>
+                <span className="btn btn-sm float-right">
+                  <EditOutlined className="text-warning" />
+                </span>
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
+    </div>
   );
 };
 
