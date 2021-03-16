df213cbb142f726694e69666dea26dd6ade18217
bharath2612 <bharathchippa49@gmail.com>
2021-03-16 21:32:46 +0530

    88. File Upload Components

--- client/src/pages/admin/product/ProductCreate.js
+++ client/src/pages/admin/product/ProductCreate.js
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 import { createProduct } from "../../../functions/product";
 import ProductCreateForm from "../../../components/forms/ProductCreateForm";
 import { getCategories, getCategorySubs } from "../../../functions/category";
-
+import FileUpload from "../../../components/forms/FileUpload"
 const initialState = {
   title: "",
   descriptioin: "",
@@ -74,6 +74,10 @@ const ProductCreate = () => {
         <div className="col-md-10">
           <h4>Product create</h4>
           <hr />
+
+          <div className="p-3">
+            <FileUpload />
+          </div>
 
           <ProductCreateForm
             handleSubmit={handleSubmit}
