333b160431ac8e4827467a84331194b6b037f8d9
bharath2612 <bharathchippa49@gmail.com>
2021-03-18 18:02:01 +0530

    96. Code Organisations

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,29 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React  from "react";
 import AdminNav from "../../components/nav/AdminNav";
-import { getProductsByCount } from "../../functions/product";
-import AdminProductCard from "../../components/cards/AdminProductCard";
+
 
 const AdminDashboard = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    loadAllProducts();
-  }, []);
-
-  const loadAllProducts = () => {
-    setLoading(true);
-    getProductsByCount(100)
-      .then((res) => {
-        setProducts(res.data);
-        setLoading(false);
-        console.log(res.data);
-      })
-      .catch((err) => {
-        setLoading(false);
-        console.log(err);
-      });
-  };
+ 
 
   return (
     <div className="container-fluid">
@@ -32,14 +12,7 @@ const AdminDashboard = () => {
           <AdminNav />
         </div>
         <div className="col">
-          <h4>All Products</h4>
-          <div className="row">
-            {products.map((product) => (
-              <div className="col-md-4" key={product._id}>
-                <AdminProductCard product={product} />
-              </div>
-            ))}
-          </div>
+          <h4>Admin Dashboard</h4>
         </div>
       </div>
     </div>
---
+++ client/src/pages/admin/product/AllProducts.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import AdminNav from "../../../components/nav/AdminNav";
+import { getProductsByCount } from "../../../functions/product";
+import AdminProductCard from "../../../components/cards/AdminProductCard";
+
+const AllProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    loadAllProducts();
+  }, []);
+
+  const loadAllProducts = () => {
+    setLoading(true);
+    getProductsByCount(100)
+      .then((res) => {
+        setProducts(res.data);
+        setLoading(false);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2">
+          <AdminNav />
+        </div>
+        <div className="col">
+          <h4>All Products</h4>
+          <div className="row">
+            {products.map((product) => (
+              <div className="col-md-4" key={product._id}>
+                <AdminProductCard product={product} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AllProducts;
