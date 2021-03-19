4d07b040f9d120aa3535abebb46ef315e84c7800
bharath2612 <bharathchippa49@gmail.com>
2021-03-19 18:40:12 +0530

    100.Product Update page

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,7 @@ import SubCreate from "./pages/admin/sub/SubCreate";
 import SubUpdate from "./pages/admin/sub/SubUpdate";
 import ProductCreate from "./pages/admin/product/ProductCreate";
 import AllProducts from "./pages/admin/product/AllProducts";
+import ProductUpdate from "./pages/admin/product/ProductUpdate";
 
 import { auth } from "./firebase";
 import { useDispatch } from "react-redux";
@@ -81,6 +82,11 @@ const App = () => {
         <AdminRoute exact path="/admin/sub" component={SubCreate} />
         <AdminRoute exact path="/admin/product" component={ProductCreate} />
         <AdminRoute exact path="/admin/products" component={AllProducts} />
+        <AdminRoute
+          exact
+          path="/admin/products/:slug"
+          component={ProductUpdate}
+        />
       </Switch>
     </>
   );
