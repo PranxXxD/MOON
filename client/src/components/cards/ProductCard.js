f799372dc4ad5dc3262014d6d91848bf34ef43df
bharath2612 <bharathchippa49@gmail.com>
2021-03-21 20:23:46 +0530

    112. Product Card using antd

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -1,5 +1,40 @@
 import React from "react";
+import { Card } from "antd";
+import { EyeOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import laptop from "../../images/laptop.jpg";
+import { Link } from "react-router-dom";
 
-const ProductCard = ({product}) => <div>{product.title}</div>;
+const { Meta } = Card;
 
+const ProductCard = ({ product }) => {
+  const { title, description, images, slug } = product;
+
+  return (
+    <Card
+      cover={
+        <img
+          src={images && images.length ? images[0].url : laptop}
+          style={{ height: "150px", objectFit: "cover" }}
+          className="p-1"
+        />
+      }
+      actions={[
+        <Link to={`/product/${slug}`}>
+          <EyeOutlined className="text-warning" /> <br /> View Produt
+        </Link>,
+        <>
+          <ShoppingCartOutlined
+            className="text-danger"
+          />
+          <br /> Add to Cart
+        </>,
+      ]}
+    >
+      <Meta
+        title={title}
+        description={`${description && description.substring(0, 40)}...`}
+      />
+    </Card>
+  );
+};
 export default ProductCard;
