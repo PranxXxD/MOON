3b4fb53308bbcdadb5b2325473bd41a97c13956b
bharath2612 <bharathchippa49@gmail.com>
2021-03-24 22:29:31 +0530

    123. View Products Page

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,7 @@ import SubUpdate from "./pages/admin/sub/SubUpdate";
 import ProductCreate from "./pages/admin/product/ProductCreate";
 import AllProducts from "./pages/admin/product/AllProducts";
 import ProductUpdate from "./pages/admin/product/ProductUpdate";
+import Product from "./pages/Product";
 
 import { auth } from "./firebase";
 import { useDispatch } from "react-redux";
@@ -87,6 +88,7 @@ const App = () => {
           path="/admin/products/:slug"
           component={ProductUpdate}
         />
+        <AdminRoute exact path="/product/:slug" component={Product} />
       </Switch>
     </>
   );
