b34867395fa9f6d8a9523f66ddb1ed1fd9e6ae3c
bharath2612 <bharathchippa49@gmail.com>
2021-03-25 18:22:48 +0530

    129. Star Rating System Backend

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -116,3 +116,41 @@ exports.productsCount = async (req, res) => {
     console.log(err);
   }
 };
+
+exports.productStar = async (req, res) => {
+  const product = await Product.findById(req.params.productId).exec();
+  const user = await User.findOne("email: req.user.email").exec();
+  const { star } = req.body;
+
+  //who is updating?
+  // check if currently logged in user have already added rating to this product ?
+  let existingRatingObject = product.ratings.find(
+    (ele) => ele.postedBy.toString() === user._id.toString()
+  );
+
+  //if user haven't left the rating yet,pust it
+  if (existingRatingObject === undefined) {
+    let ratingAdded = await Product.findByIdAndUpdate(
+      product._id,
+      {
+        $push: { ratings: { star, objectId: user._id } },
+      },
+      {
+        new: true,
+      }
+    ).exec();
+    console.log("rating added", ratingAdded);
+    res.json(ratingAdded);
+  } else {
+    //if user have already left the rating, update it
+    const ratingUpdated = await Product.updateOne(
+      {
+        ratings: { $elemMatch: existingRatingObject },
+      },
+      { $set: { "ratings.$.star": star } },
+      { new: true }
+    ).exec();
+    console.log("rating updated", ratingUpdated);
+    res.json(ratingUpdated);
+  }
+};
--- server/routes/product.js
+++ server/routes/product.js
@@ -13,6 +13,7 @@ const {
   update,
   list,
   productsCount,
+  productStar,
 } = require("../controllers/product");
 
 //routes
@@ -23,6 +24,9 @@ router.get("/products/:count", listAll);
 router.delete("/product/:slug", authCheck, adminCheck, remove);
 router.get("/product/:slug", read);
 router.put("/product/:slug", authCheck, adminCheck, update);
+
 router.post("/products", list);
+
+router.put("/product/star/:productId", authCheck, productStar);
 
 module.exports = router;
