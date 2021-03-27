605d4e3dd2e78c3af01735ed44980e95b00bb2c7
bharath2612 <bharathchippa49@gmail.com>
2021-03-27 17:34:57 +0530

    143. Category Page wth products

--- client/src/pages/category/CategoryHome.js
+++ client/src/pages/category/CategoryHome.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { getCategory } from "../../functions/category";
 import { Link } from "react-router-dom";
 import ProductCard from "../../components/cards/ProductCard";
+import CategoryList from "../../components/category/CategoryList";
 
 const CategoryHome = ({ match }) => {
   const [category, setCategory] = useState([]);
@@ -12,13 +13,38 @@ const CategoryHome = ({ match }) => {
 
   useEffect(() => {
     setLoading(true);
-    getCategory(slug).then((c) => {
-      console.log(JSON.stringify(c.data, null, 4));
-      setCategory(c.data);
+    getCategory(slug).then((res) => {
+      console.log(JSON.stringify(res.data, null, 4));
+      setCategory(res.data.category);
+      setProducts(res.data.products);
+      setLoading(false);
     });
   }, []);
 
-  return <div>{slug}</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          {loading ? (
+            <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+              Loading...
+            </h4>
+          ) : (
+            <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+              Products in "{category.name}" Category
+            </h4>
+          )}
+        </div>
+      </div>
+      <div className="row">
+        {products.map((p) => (
+          <div className="col" key={p._id}>
+            <ProductCard product={p} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default CategoryHome;
