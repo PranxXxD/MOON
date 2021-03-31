7f93592e361ff84e4d7897b71df185bee204a8c1
bharath2612 <bharathchippa49@gmail.com>
2021-03-31 13:36:58 +0530

    159. FIlter by brand frontend

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -7,7 +7,7 @@ import { getCategories } from "../functions/category";
 import { getSubs } from "../functions/sub";
 import { useSelector, useDispatch } from "react-redux";
 import ProductCard from "../components/cards/ProductCard";
-import { Menu, Slider, Checkbox } from "antd";
+import { Menu, Slider, Checkbox, Radio } from "antd";
 import {
   DollarOutlined,
   DownSquareOutlined,
@@ -27,6 +27,14 @@ const Shop = () => {
   const [star, setStar] = useState([]);
   const [subs, setSubs] = useState([]);
   const [sub, setSub] = useState([]);
+  const [brands, setBrands] = useState([
+    "Apple",
+    "Samsung",
+    "Microsoft",
+    "Lenovo",
+    "ASUS",
+  ]);
+  const [brand, setBrand] = useState("");
 
   let dispatch = useDispatch();
   let { search } = useSelector((state) => ({ ...state }));
@@ -149,6 +157,25 @@ const Shop = () => {
       </div>
     ));
 
+  //show products based on Brands
+  const showBrands = () =>
+    brands.map((b) => (
+      <Radio
+        value={b}
+        name={b}
+        checked={b === brand}
+        onChange={handleBrand}
+        className="pb-1 pl-4 pr-4"
+      >
+        {b}
+      </Radio>
+    ));
+
+  const handleBrand = (e) => {
+    setBrand(e.target.value);
+    fetchProducts({ brand: e.target.value });
+  };
+
   return (
     <div className="container-fluid">
       <div className="row">
@@ -156,7 +183,7 @@ const Shop = () => {
           <h4>Search/Filter</h4>
           <hr />
 
-          <Menu defaultOpenKeys={["1", "2", "3", "4"]} mode="inline">
+          <Menu defaultOpenKeys={["1", "2", "3", "4", "5"]} mode="inline">
             {/* price */}
             <SubMenu
               key="1"
@@ -213,6 +240,20 @@ const Shop = () => {
             >
               <div style={{ marginTop: "-10px" }} className="pl-4 pr-4">
                 {showSubs()}
+              </div>
+            </SubMenu>
+
+            {/* Brands */}
+            <SubMenu
+              key="5"
+              title={
+                <span className="h6">
+                  <DownSquareOutlined /> Brands
+                </span>
+              }
+            >
+              <div style={{ marginTop: "-10px" }} className="pr-5">
+                {showBrands()}
               </div>
             </SubMenu>
           </Menu>
