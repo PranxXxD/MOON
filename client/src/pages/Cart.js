6754ae56de6d992fe419229fb434013c89a8887a
bharath2612 <bharathchippa49@gmail.com>
2021-04-01 00:54:11 +0530

    168. current page order summarry

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -6,11 +6,51 @@ const Cart = () => {
   const { user, cart } = useSelector((state) => ({ ...state }));
   const dispatch = useDispatch();
 
-  
+  const getTotal = () => {
+    return cart.reduce((currentVal, nextVal) => {
+      return currentVal + nextVal.count * nextVal.price;
+    }, 0);
+  };
+
   return (
-    <div className="container-fluid">
+    <div className="container-fluid pt-2">
       <div className="row">
-        <h4>Cart</h4>
+        <div className="col-md-8">
+          <h4>Cart / {cart.length}</h4>
+          {!cart.length ? (
+            <p>
+              No products in cart. <Link to="/shop">Continue Shopping</Link>
+            </p>
+          ) : (
+            "Show cart items"
+          )}
+        </div>
+        <div className="col-md-4">
+          <h4>Order Summary</h4>
+          <hr />
+          <p>Products</p>
+          {cart.map((c, i) => (
+            <div key={i}>
+              <p>
+                {c.title} x {c.count} = ₹{c.price * c.count}
+              </p>
+            </div>
+          ))}
+          <hr />
+          <h6>
+            Total: <strong>₹{getTotal()}</strong>
+          </h6>
+          <hr />
+          {user ? (
+            <button className="btn btn-sm btn-primary mt-2">
+              Proceed to Checkout
+            </button>
+          ) : (
+            <button className="btn btn-sm btn-primary mt-2">
+              Login to Checkout
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
