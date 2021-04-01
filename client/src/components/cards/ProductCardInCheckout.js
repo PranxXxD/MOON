fe25af3e910f8dace0325ed2363e7f48f83d1358
bharath2612 <bharathchippa49@gmail.com>
2021-04-01 17:30:32 +0530

    170. Cart items in table

---
+++ client/src/components/cards/ProductCardInCheckout.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ProductCardInCheckout = ({ p }) => {
+  return (
+    <tbody>
+      <tr>
+        <td>{p.title}</td>
+        <td>{p.price}</td>
+        <td>{p.brand}</td>
+        <td>{p.color}</td>
+        <td>{p.count}</td>
+        <td>Shipping</td>
+        <td>Delete Icon</td>
+      </tr>
+    </tbody>
+  );
+};
+
+export default ProductCardInCheckout;
