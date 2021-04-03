c2c5942a0b3595085223d115e5afd9cd0ff7e3a1
bharath2612 <bharathchippa49@gmail.com>
2021-04-03 16:45:45 +0530

    180. Save cart to database

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/user", (req, res) => {
-  res.json({
-    data: "I AM USER!",
-  });
-});
+//middlewares
+const { authCheck } = require("../middlewares/auth");
+
+//controllers
+const { userCart } = require("../controllers/user");
+
+router.post("/cart", authCheck, userCart); //save cart
+// router.get("/user", (req, res) => {
+//   res.json({
+//     data: "I AM USER!",
+//   });
+// });
 
 module.exports = router;
