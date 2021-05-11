9150ed2b3d62d36bc150d5ea22ea84b6c183cf0d
bharath2612 <bharathchippa49@gmail.com>
2021-05-11 17:23:49 +0530

    homepage carousel

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Jumbotron from "../components/cards/Jumbotron";
+import CarouselContainer from "../components/Carousel/CarouselContainer";
 import NewArrivals from "../components/home/NewArrivals";
 import BestSellers from "../components/home/BestSellers";
 import CategoryList from "../components/category/CategoryList";
@@ -9,7 +9,9 @@ const Home = () => {
   return (
     <>
       <div className="jumbotron text-danger h1 font-weight-bold text-center">
-        <Jumbotron text={["Latest Arrivals", "New Arrivals", "Best Sellers"]} />
+        <CarouselContainer
+          // text={["Latest Arrivals", "New Arrivals", "Best Sellers"]}
+        />
       </div>
 
       <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
