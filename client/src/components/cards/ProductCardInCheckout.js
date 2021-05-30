b651358df73f054a5e75c9e04a93c21a17787023
bharath2612 <bharathchippa49@gmail.com>
2021-05-31 03:00:18 +0530

    cart page fix

--- client/src/components/cards/ProductCardInCheckout.js
+++ client/src/components/cards/ProductCardInCheckout.js
@@ -1,20 +1,17 @@
-import React from "react";
-import ModalImage from "react-modal-image";
+import React, { useState } from "react";
 import laptop from "../../images/laptop.jpg";
 import { useDispatch } from "react-redux";
 import { toast } from "react-toastify";
-import { CheckCircleOutlined, CloseCircleOutlined } from "@ant-design/icons";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableRow from "@material-ui/core/TableRow";
 import Select from "@material-ui/core/Select";
-import CheckBoxIcon from "@material-ui/icons/CheckBox";
-import CancelIcon from "@material-ui/icons/Cancel";
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
-import { withStyles, makeStyles } from "@material-ui/core/styles";
+import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
 
-const ProductCardInCheckout = ({ p }) => {
+const ProductCardInCheckout = ({ p, wrap, handleChange }) => {
   const colors = ["Black", "Brown", "Silver", "White", "Blue"];
+
   let dispatch = useDispatch();
 
   const handleColorChange = (e) => {
@@ -87,102 +84,87 @@ const ProductCardInCheckout = ({ p }) => {
       });
     }
   };
-  const StyledTableCell = withStyles((theme) => ({
-    head: {
-      backgroundColor: theme.palette.common.black,
-      color: theme.palette.common.white,
-    },
-    body: {
-      fontSize: 14,
-    },
-  }))(TableCell);
-
-  const StyledTableRow = withStyles((theme) => ({
-    root: {
-      "&:nth-of-type(odd)": {
-        backgroundColor: theme.palette.action.hover,
-      },
-    },
-  }))(TableRow);
-
-  function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-  }
-
-  const rows = [
-    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-    createData("Eclair", 262, 16.0, 24, 6.0),
-    createData("Cupcake", 305, 3.7, 67, 4.3),
-    createData("Gingerbread", 356, 16.0, 49, 3.9),
-  ];
-
-  const useStyles = makeStyles({
-    table: {
-      minWidth: 350,
-    },
-  });
-  const classes = useStyles();
 
   return (
-    <TableBody>
-      <StyledTableRow>
-        <StyledTableCell
-          style={{ width: "200px", height: "auto" }}
-          component="th"
-          scope="row"
-        >
-          {p.images.length ? (
-            <ModalImage small={p.images[0].url} large={p.images[0].url} />
-          ) : (
-            <ModalImage small={laptop} large={laptop} />
-          )}
-        </StyledTableCell>
-        <StyledTableCell align="center">{p.title}</StyledTableCell>
-        <StyledTableCell align="center">₹{p.price}</StyledTableCell>
-        <StyledTableCell align="center">
-          <Select native value={p.color} onChange={handleColorChange}>
-            {p.color ? (
-              <option value={p.color}>{p.color}</option>
-            ) : (
-              <option>Select</option>
-            )}
-            {colors
-              .filter((c) => c !== p.color)
-              .map((c) => (
-                <option key={c} value={c}>
-                  {c}
-                </option>
-              ))}
-          </Select>
-        </StyledTableCell>
-        <StyledTableCell align="center" size="small" padding="none">
-          <Select native value={p.count} onChange={handleQuantityChange}>
-            <option aria-label="None" value="" />
-            <option value={1}>1</option>
-            <option value={2}>2</option>
-            <option value={3}>3</option>
-            <option value={4}>4</option>
-            <option value={5}>5</option>
-            <option value={6}>6</option>
-          </Select>
-        </StyledTableCell>
-        <StyledTableCell align="center">
-          {p.shipping === "Yes" ? (
-            <CheckBoxIcon fontSize="large" className="text-success" />
-          ) : (
-            <CancelIcon fontSize="large" className="text-danger" />
-          )}
-        </StyledTableCell>
-        <StyledTableCell align="center">
-          <DeleteForeverIcon
-            fontSize="large"
-            className="text-danger pointer"
-            onClick={() => handleRemove(p._id)}
-          />
-        </StyledTableCell>
-      </StyledTableRow>
-    </TableBody>
+    <div className="item-box">
+      <div className="item-details">
+        <Container>
+          <Row className="mb-1 align-items-center">
+            <Col xs="12" md="9">
+              <div className="d-flex align-items-center">
+                <img
+                  className="item-image"
+                  src={`${p.images[0] ? p.images[0].url : laptop}`}
+                />
+                <Link
+                  to={`/product/${p.slug}`}
+                  className="item-link one-line-ellipsis"
+                >
+                  <h1 className="item-name one-line-ellipsis">{p.title}</h1>
+                  <p className="item-name one-line-ellipsis">
+                    ₹{p.price * p.count}
+                  </p>
+                </Link>
+              </div>
+            </Col>
+            <Col xs="3" md="3" className="text-top"></Col>
+          </Row>
+          <Row className="mb-2 align-items-center">
+            <Col xs="9"></Col>
+          </Row>
+          <Row className="mb-2 align-items-center">
+            <Col xs="3" md="2">
+              <p className="item-label">qty:</p>
+              <Select native value={p.count} onChange={handleQuantityChange}>
+                <option aria-label="None" value="" />
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={5}>5</option>
+                <option value={6}>6</option>
+              </Select>
+            </Col>
+            <Col xs="5" md="3" className="text-left">
+              <p className="item-label">Color:</p>
+              <Select native value={p.color} onChange={handleColorChange}>
+                {p.color ? (
+                  <option value={p.color}>{p.color}</option>
+                ) : (
+                  <option>Select</option>
+                )}
+                {colors
+                  .filter((c) => c !== p.color)
+                  .map((c) => (
+                    <option key={c} value={c}>
+                      {c}
+                    </option>
+                  ))}
+              </Select>
+            </Col>
+            <Col xs="3" md="5">
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={wrap}
+                    onChange={handleChange}
+                    name="gift"
+                  />
+                }
+                label="Wrap your Gift?"
+              />
+            </Col>
+            <Col xs="3" md="2">
+              <DeleteForeverIcon
+                fontSize="large"
+                className="text-danger pointer"
+                onClick={() => handleRemove(p._id)}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
   );
 };
 
--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,21 +1,16 @@
-import React, { useState} from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import ProductCardInCheckout from "../components/cards/ProductCardInCheckout";
 import { userCart } from "../functions/user";
 import Button from "../components/Button";
-import Table from "@material-ui/core/Table";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
-import { withStyles, makeStyles } from "@material-ui/core/styles";
+import { Row, Col } from "reactstrap";
 
 const Cart = ({ history }) => {
-  const [shipping, setShipping] = useState(0);
   const { user, cart } = useSelector((state) => ({ ...state }));
-  const dispatch = useDispatch();
+  const [wrap, setWrap] = useState({
+    gift: false,
+  });
 
   const getTotal = () => {
     return cart.reduce((currentVal, nextVal) => {
@@ -60,49 +55,31 @@ const Cart = ({ history }) => {
       .catch((err) => console.log("save data error", err));
   };
 
-  const StyledTableCell = withStyles((theme) => ({
-    head: {
-      backgroundColor: theme.palette.common.black,
-      color: theme.palette.common.white,
-    },
-    body: {
-      fontSize: 14,
-    },
-  }))(TableCell);
-
-  const useStyles = makeStyles({
-    table: {
-      minWidth: 350,
-    },
-  });
-  const classes = useStyles();
+  const handleChange = (e) => {
+    setWrap({ ...wrap, [e.target.name]: e.target.checked });
+  };
 
   const showCartItems = () => (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Product</StyledTableCell>
-            <StyledTableCell align="center">Name</StyledTableCell>
-            <StyledTableCell align="center">Price;</StyledTableCell>
-            <StyledTableCell align="center">Color</StyledTableCell>
-            <StyledTableCell align="center">Quantity</StyledTableCell>
-            <StyledTableCell align="center">Shipping</StyledTableCell>
-            <StyledTableCell align="center">Remove</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        {cart.map((p) => (
-          <ProductCardInCheckout key={p._id} p={p} />
-        ))}
-      </Table>
-    </TableContainer>
+    <div className="cart mt-4">
+      <div className="cart-body">
+        <div className="cart-list">
+          {cart.map((p) => (
+            <ProductCardInCheckout
+              wrap={wrap.gift}
+              handleChange={handleChange}
+              key={p._id}
+              p={p}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
   );
 
   return (
-    <div className="container-fluid pt-2">
-      <div className="row">
-        <div className="col-md-8 d-inline-block">
-          <h4 className="h_4">Cart / {cart.length}</h4>
+    <div className="contact">
+      <Row>
+        <Col xs="12" sm="12" md="6">
           {!cart.length ? (
             <p className="h_4">
               No products in cart. <Link to="/shop">Continue Shopping</Link>
@@ -110,47 +87,60 @@ const Cart = ({ history }) => {
           ) : (
             showCartItems()
           )}
-        </div>
-        <div className="col-md-4">
-          <h4 className="h_4">Order Summary</h4>
-          <hr />
-          <p className="h_4">Products</p>
-          {cart.map((c, i) => (
-            <div key={i}>
-              <p>
-                {c.title} x {c.count} = ₹{c.price * c.count}
-              </p>
+        </Col>
+        <Col xs="12" sm="12" md="3">
+          <Col xs="12" sm="12" xs="12">
+            <div className="cart mt-4">
+              <div className="cart-body">
+                <div className="item-details">
+                  <div className="item-box">
+                    <h4 className="h_4">Order Summary</h4>
+                    <hr />
+                    {cart.map((c, i) => (
+                      <div key={i}>
+                        <p className="item-label">
+                          {c.title} x {c.count}
+                        </p>
+                      </div>
+                    ))}
+                    <hr />
+                    <p className="item-label">Order Total : ₹{getTotal()}</p>
+                    <p className="item-label">
+                      Convience fee: ₹{getShipping(getWeight)}
+                    </p>
+                    <hr />
+                    <p className="item-name one-line-ellipsis">
+                      Total: ₹{getTotal() + getShipping(getWeight)}
+                    </p>
+                    {user ? (
+                      <Button
+                        variant="primary"
+                        text="Checkout"
+                        onClick={saveOrderToDb}
+                        className="w-100 mt-2"
+                        disabled={!cart.length}
+                      />
+                    ) : (
+                      <Link
+                        to={{
+                          pathname: "/login",
+                          state: { from: "cart" },
+                        }}
+                      >
+                        <Button
+                          className="w-100"
+                          text="Login to Checkout mt-2"
+                          variant="primary"
+                        />
+                      </Link>
+                    )}
+                  </div>
+                </div>
+              </div>
             </div>
-          ))}
-          <hr />
-          <p>Order Total : ₹{getTotal()}</p>
-          <p>Shipping Charges: ₹{getShipping(getWeight)}</p>
-          <hr />
-          <h4>Total: ₹{getTotal() + getShipping(getWeight)}</h4>
-          <hr />
-          {user ? (
-            <Button
-              variant="primary"
-              onClick={saveOrderToDb}
-              className="btn"
-              text="Proceed to Checkout"
-              disabled={!cart.length}
-            ></Button>
-          ) : (
-            <Button className="btn">
-              <Link
-                className="link"
-                to={{
-                  pathname: "/login",
-                  state: { from: "cart" },
-                }}
-              >
-                Login to Checkout
-              </Link>
-            </Button>
-          )}
-        </div>
-      </div>
+          </Col>
+        </Col>
+      </Row>
     </div>
   );
 };
