5b668021c55e4b16702614d6bbec97c925fcf7a6
bharath2612 <bharathchippa49@gmail.com>
2021-06-14 14:16:16 +0530

    hnadle coupon on x icon

--- client/src/pages/Cart/Summary.js
+++ client/src/pages/Cart/Summary.js
@@ -249,7 +249,7 @@ const Summary = ({
                         fontSize="small"
                         value={on}
                         className="text-danger pointer ml-1"
-                        onClick={handleCoupon}
+                        onClick={}
                       />
                     </p>
                   )
